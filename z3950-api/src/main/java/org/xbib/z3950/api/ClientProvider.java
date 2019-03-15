a44c1b38a9f401fc87043af6d4edef09c5ce8d60
Jörg Prante <joergprante@gmail.com>
2019-03-15 16:45:18 +0100

    docs

--- z3950-api/src/main/java/org/xbib/z3950/api/ClientProvider.java
+++ z3950-api/src/main/java/org/xbib/z3950/api/ClientProvider.java
@@ -1,5 +1,9 @@
 package org.xbib.z3950.api;
 
+/**
+ * Client provider.
+ * @param <C> the client type parameter
+ */
 public interface ClientProvider<C extends Client> {
 
     C getClient();
---
+++ z3950-api/src/main/java/org/xbib/z3950/api/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * API for Z39.50 applications.
+ */
+package org.xbib.z3950.api;
